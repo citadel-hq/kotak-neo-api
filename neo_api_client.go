425b940d7cf54726ef28114b0e24da61fbb2e9b1
Uddeshya Singh <singhuddeshyaofficial@gmail.com>
2024-07-09 09:20:47 +0530

    Porting more APIs.

--- neo_api_client.go
+++ neo_api_client.go
@@ -10,11 +10,15 @@ import (
 )
 
 type KotakClient struct {
-	config         api.Configuration
-	websocket      *websocket.HSWrapper // TODO: think about abstracting this away from the wrapper and building a simpler class instead.
-	loginAPI       *api.LoginAPI
-	orderAPI       *api.OrderService
-	orderReportAPI *api.OrderReportAPI
+	config          api.Configuration
+	websocket       *websocket.HSWrapper // TODO: think about abstracting this away from the wrapper and building a simpler class instead.
+	loginAPI        *api.LoginAPI
+	orderAPI        *api.OrderService
+	orderReportAPI  *api.OrderReportAPI
+	orderHistoryAPI *api.OrderHistoryService
+	tradeReportAPI  *api.TradeReportAPI
+	positionsAPI    *api.PositionsAPI
+	holdingsAPI     *api.PortfolioHoldingsAPI
 }
 
 // note: a broker interface should give you a channel for consumption and a client for placing orders.
@@ -24,12 +28,20 @@ func NewKotakClient(configuration api.Configuration) (*KotakClient, chan websock
 	loginAPI := api.NewLoginAPI(apiClient)
 	orderAPI := api.NewOrderService(apiClient)
 	orderReportAPI := api.NewOrderReportAPI(apiClient)
+	orderHistoryAPI := api.NewOrderHistoryService(apiClient)
+	tradeReportAPI := api.NewTradeReportAPI(apiClient)
+	positionsAPI := api.NewPositionsAPI(apiClient)
+	holdingsAPI := api.NewPortfolioHoldingsAPI(apiClient)
 	return &KotakClient{
-		config:         configuration,
-		websocket:      ws,
-		loginAPI:       loginAPI,
-		orderAPI:       orderAPI,
-		orderReportAPI: orderReportAPI,
+		config:          configuration,
+		websocket:       ws,
+		loginAPI:        loginAPI,
+		orderAPI:        orderAPI,
+		orderReportAPI:  orderReportAPI,
+		orderHistoryAPI: orderHistoryAPI,
+		tradeReportAPI:  tradeReportAPI,
+		positionsAPI:    positionsAPI,
+		holdingsAPI:     holdingsAPI,
 	}, wsChannel
 }
 
@@ -139,3 +151,53 @@ func (c *KotakClient) OrderReport() (map[string]interface{}, error) {
 		},
 	}, errors.New("please complete the 2FA process before placing orders")
 }
+
+func (c *KotakClient) OrderHistory(orderId string) (map[string]interface{}, error) {
+	if c.config.EditToken != "" && c.config.EditSid != "" {
+		err := api.OrderHistoryValidation(orderId)
+		if err != nil {
+			return nil, err
+		}
+		return c.orderHistoryAPI.FetchOrderHistory(orderId)
+	}
+	return map[string]interface{}{
+		"error": []map[string]string{
+			{"code": "900", "message": "Complete the 2fa process before accessing this application"},
+		},
+	}, errors.New("please complete the 2FA process before placing orders")
+}
+
+func (c *KotakClient) TradeReport(orderId string) (map[string]interface{}, error) {
+	if c.config.EditToken != "" && c.config.EditSid != "" {
+		return c.tradeReportAPI.GetTradeReport(orderId)
+	}
+	return map[string]interface{}{
+		"error": []map[string]string{
+			{"code": "900", "message": "Complete the 2fa process before accessing this application"},
+		},
+	}, errors.New("please complete the 2FA process before placing orders")
+}
+
+func (c *KotakClient) Positions() (map[string]interface{}, error) {
+	if c.config.EditToken != "" && c.config.EditSid != "" {
+		return c.positionsAPI.GetPositions()
+	}
+	return map[string]interface{}{
+		"error": []map[string]string{
+			{"code": "900", "message": "Complete the 2fa process before accessing this application"},
+		},
+	}, errors.New("please complete the 2FA process before placing orders")
+}
+
+func (c *KotakClient) Holdings() (map[string]interface{}, error) {
+	if c.config.EditToken != "" && c.config.EditSid != "" {
+		return c.holdingsAPI.Fetch()
+	}
+	return map[string]interface{}{
+		"error": []map[string]string{
+			{"code": "900", "message": "Complete the 2fa process before accessing this application"},
+		},
+	}, errors.New("please complete the 2FA process before placing orders")
+}
+
+// TODO: build margin_required, modify_order etc.
